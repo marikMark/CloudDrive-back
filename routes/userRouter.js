cff350a2b895ecb5cddf87141892a96cb23af48a
Mark Novikov <pradobeatz1@gmail.com>
2021-07-17 18:04:13 +0300

    jvnkjsdv

--- routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const userController = require('../controllers/userController');
-const authMiddleware = require('../middleware/auth.middleware');
-
-router.get('/login', userController.checkUser);
-router.get('/check', authMiddleware, userController.checkUser);
-
-module.exports = router;
