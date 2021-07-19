8050a12f80e28a0e772898d3367d34ae4614d860
Mark Novikov <pradobeatz1@gmail.com>
2021-07-19 22:05:09 +0300

    removing sleep

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,13 +11,9 @@ class AuthController {
         const ipAddress = req.ip.split(':').pop();
         const userId = uuid.v4();
 
-        function sleep(ms) {
-            return new Promise(resolve => setTimeout(resolve, ms));
-        }
         const candidate = await User.findOne({where: {ipAddress}});
         if(candidate) {
             const token = jwt.sign({userId: candidate.userId}, process.env.SECRET_KEY, {expiresIn: '1w'});
-            await sleep(1000);
             return await res.json({user: candidate, token});
         }
 
